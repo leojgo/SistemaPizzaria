92f1112567a4c8036b478b90f52ad3ee3a892f1f
Leonardo Oliveira <leonardo.oliveira@integritas.net>
2018-04-23 00:19:03 -0300

    Remover campos não utilizados de pedidos

--- Sistema/WpfView/ListaPedidos.xaml.cs
+++ Sistema/WpfView/ListaPedidos.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using Controllers;
 using Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfView
 {
@@ -26,11 +15,11 @@ namespace WpfView
             InitializeComponent();
             MostrarGrid();
         }
-        
+
         private void MostrarGrid()
         {
-            List<Pedido> list=PedidoController.ListarTodosPedidos();
-            if (list!=null)
+            List<Pedido> list = PedidoController.ListarTodosPedidos();
+            if (list != null)
             {
                 gridPedidos.ItemsSource = list;
             }
